e23bdcfe7073796d35750a2ad482f74a16ecdccc
Isaías Fernandes <isaiasnf02@gmail.com>
2023-03-30 15:09:17 -0300

    Exercício do banco Trybe

--- 01-fundamentos/secao-03-introducao-a-javaScript/dia-03-javaScript-funcoes/paraFixar1.js
+++ 01-fundamentos/secao-03-introducao-a-javaScript/dia-03-javaScript-funcoes/paraFixar1.js
@@ -1,1 +1,19 @@
-Olá;
+let saldo = 9300;
+
+function somaSaldo(valor) {
+  return valor + saldo;
+};
+function subtraiSaldo(valor) {
+  return saldo - valor;
+};
+function multiplicaSaldo(valor) {
+  return valor * saldo;
+};
+function divideSaldo(valor) {
+  return saldo / valor;
+};
+
+console.log(somaSaldo(600));
+console.log(subtraiSaldo(70));
+console.log(multiplicaSaldo(2));
+console.log(divideSaldo(3));
